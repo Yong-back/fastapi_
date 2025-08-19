17b2348f0d33335f05df6b08da54bb47731ec9ae
Yong-back <skathd6406@gamil.com>
2025-08-19 10:16:48 +0900

    install mypy

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# int, str, bool
+# dict, set, list, tuple -> collction (자료형)
+
 
 @app.get("/")
-async def root():
+async def root() -> dict[str, str]:
     return {"message": "Hello World"}
 
 
 @app.get("/hello/{name}")
-async def say_hello(name: str):
+async def say_hello(name: str) -> dict[str, str]:
     return {"message": f"Hello {name}"}
